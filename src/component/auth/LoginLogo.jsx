5f71d5ab6134d7a85bcd5301c1a5d8a525337301
0625yt <yt01234@naver.com>
2025-04-13 09:05:12 +0900

    폴더 관련 생성 기능 추가

---
+++ src/component/auth/LoginLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginLogo = () => {
+  return (
+    <div className="logo">
+      <img src="/login_image/Logo.png" alt="Logo" />
+    </div>
+  );
+};
+
+export default LoginLogo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './component/auth/App.css'; // App.css도 함께 루트로 이동
+import App from './App'; // 경로를 수정
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
